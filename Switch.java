06e52e809f43cc6f8a947a52deb05d5bfddd11ea
Zwe Marn Htet <mybusinessf16@gmail.com>
2026-04-04 21:42:22 +0630

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,56 @@
+package Lessons2;
+
+import java.util.Scanner;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner=new Scanner(System.in);
+		
+		System.out.println("Enter weekend :");
+		int day=scanner.nextInt();
+		
+		
+		
+		switch(day) {
+		
+		case 1:
+			System.out.println("Monday");
+			break;
+			
+			
+		case 2:
+			System.out.println("Tuesday");
+			break;
+			
+			
+		case 3:
+			System.out.println("Wednesday");
+			break;
+			
+		case 4:
+			System.out.println("Thursday");
+			break;
+			
+		case 5:
+			System.out.println("Friday");
+			
+		case 6:
+			System.out.println("Saturday");
+			break;
+			
+		case 7:
+			System.out.println("Sunday");
+			break;
+			
+			
+			default:
+				System.out.println("Happy Weekend");
+		
+		
+		}
+
+	}
+
+}
